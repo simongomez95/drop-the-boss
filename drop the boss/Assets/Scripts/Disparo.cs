6aa9952f65d7b70e33c9bb5ba7fe5f5d8ebb9267
czulua18 <camilozv18@gmail.com>
2017-01-20 21:30:40 -0500

    proyecto Master race

---
+++ drop the boss/Assets/Scripts/Disparo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour {
+
+
+	public bool algo;
+	public Rigidbody nota;
+	public float speed;
+	void Start () {
+		InvokeRepeating ("lanzaNotas",2f,5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(algo){
+//			Invoke ("lanzaNotas",2f);
+//		}
+	}
+
+	private void lanzaNotas(){
+		Rigidbody instantiatedProjectile = Instantiate(nota,transform.position, transform.rotation ) as Rigidbody;
+		instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(speed, 0f,0f));
+	}
+}
