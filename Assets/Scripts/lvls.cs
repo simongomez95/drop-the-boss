6e101a497fc3d25353fc097963e73b89cd13eb8f
czulua18 <camilozv18@gmail.com>
2017-01-21 16:33:51 -0500

    vnxfgvhbmjk,l;./

---
+++ Assets/Scripts/lvls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+public class lvls : MonoBehaviour {
+
+	int lvl;
+	// Use this for initialization
+	void Start () {
+		lvl = SceneManager.GetActiveScene ().buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void nextLvl(){
+		EditorSceneManager.LoadScene (lvl+1);
+	}
+}
---
+++ Assets/Scripts/progreso.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class progreso : MonoBehaviour {
+
+	float tiempo;
+	public float vel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tiempo += Time.deltaTime;
+		print (tiempo);
+		if (tiempo < 82.1f) {
+			transform.position = new Vector2 (transform.position.x + vel * Time.deltaTime, transform.position.y);
+		} else {
+			transform.position = new Vector2 (transform.position.x, transform.position.y);
+		}
+
+	}
+}
