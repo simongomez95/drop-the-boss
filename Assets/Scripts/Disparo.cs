7a2373fa3e4be24d43f7597cbe6f27a6d5851b03
simongomez95 <simongomez95@gmail.com>
2017-01-20 23:32:58 -0500

    Creacion de notas por tiempo

---
+++ Assets/Scripts/Disparo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disparo : MonoBehaviour {
+
+
+	public bool algo;
+	public Rigidbody nota;
+	public float speed;
+	private float timer;
+	private List<int> listaTiempos;
+	void Start () {
+		Dictionary<int, string> notas = new Dictionary<int, string>();
+		notas.Add(2, "nota1");
+		notas.Add(20, "nota1");
+		notas.Add(3, "nota1");
+		notas.Add(10, "nota1");
+		listaTiempos = new List<int>(notas.Keys);
+		listaTiempos.Sort();
+		this.timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		foreach (int tiempoNota in listaTiempos)
+		{
+			Debug.Log(this.timer);
+			Debug.Log((this.timer < tiempoNota + 1/2) && (this.timer > tiempoNota - 1/2));
+			if((this.timer - 0.798722 < tiempoNota ) && (this.timer + 0.798722 > tiempoNota)) {
+				lanzaNotas();
+			}
+		}
+	}
+
+	private void lanzaNotas(){
+		Rigidbody instantiatedProjectile = Instantiate(nota,transform.position, transform.rotation ) as Rigidbody;
+		instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(speed, 0f,0f));
+	}
+}
