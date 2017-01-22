a3811b095ba1e59b2d4a6aa77d7eed7acfb373c3
czulua18 <camilozv18@gmail.com>
2017-01-21 22:51:44 -0500

    bug

--- Assets/Scripts/animator.cs
+++ Assets/Scripts/animator.cs
@@ -12,21 +12,23 @@ public class animator : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown("f") || Input.GetKeyDown("g") || Input.GetKeyDown("h") || Input.GetKeyDown("j")){
+		if (Input.GetKeyDown ("f") || Input.GetKeyDown ("g") || Input.GetKeyDown ("h") || Input.GetKeyDown ("j")) {
 			anim.SetTrigger ("flautaUp");
 		}
 
-		if(Input.GetKeyUp("f") || Input.GetKeyUp("g") || Input.GetKeyUp("h") || Input.GetKeyUp("j")){
-			StartCoroutine(Esperar());
+		if (Input.GetKeyUp ("f") || Input.GetKeyUp ("g") || Input.GetKeyUp ("h") || Input.GetKeyUp ("j")) {
+			StartCoroutine (Esperar ());
 			anim.SetTrigger ("flautaDown");
-			StartCoroutine(Esperar());
+			StartCoroutine (Esperar ());
 			anim.SetTrigger ("corrida");
 			
+		}
+
 	}
 
 	IEnumerator Esperar() {
-        print(Time.time);
-        yield return new WaitForSeconds(0.5f);
-        print(Time.time);
-    }
+		print(Time.time);
+		yield return new WaitForSeconds(0.5f);
+		print(Time.time);
+	}
 }
